03d348eb0e5ce997edb68d5390ee39d418cd88ec
yanmingshuang <2314318218@qq.com>
2020-08-18 11:50:00 +0800

    样式的三种写法

--- src/pages/home/cookbook/Header.jsx
+++ src/pages/home/cookbook/Header.jsx
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 import {HeaderWrap} from "./styledCookbook";
-
+import './cookbook.css'
 class Header extends Component {
     render() {
         return (
             <div>
+                <div className="headerClass">
+                    className形式的样式
+                </div>
+                {/*引入styled*插件形式的样式写法*/}
                 <HeaderWrap>
-                    美食大全
+                    引入styled插件形式的样式写法
                 </HeaderWrap>
+                <div style={{height: '2.44rem',
+                    background: '#ee742f',
+                    lineHeight: '2.44rem',
+                    textAlign: 'center',
+                    color: '#fff',
+                    fontSize: '1.18rem'}}>内联样式的写法</div>
             </div>
         );
     }
